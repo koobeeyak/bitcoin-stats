0a13cd08abb48c67b1723da764f6805c04a13ee6
tucobenedicto <phkubiak@gmail.com>
2018-11-18 19:05:55 -0500

    make start end dates param args

--- app.py
+++ app.py
@@ -10,8 +10,7 @@ app = Flask(__name__)
 api = Api(app)
 api_version = 'v1'
 
-api_url = 'api/{}'.format(api_version)
-api.add_resource(BitcoinStats, '/{}/bitcoin_stats/<start_date>/<end_date>'.format(api_url))
+api.add_resource(BitcoinStats, '/api/{}/bitcoin_stats'.format(api_version))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
--- resources/bitcoin_stats.py
+++ resources/bitcoin_stats.py
@@ -1,16 +1,26 @@
 # 3p
-from flask_restful import Resource
+from flask_restful import Resource, reqparse
 
 # project
 from models.bitcoin_stat import BitcoinStat
 
 
 class BitcoinStats(Resource):
-    def get(self, start_date, end_date):
+    def get(self):
+        parser = reqparse.RequestParser()
+        parser.add_argument('start_date')
+        parser.add_argument('end_date')
+        args = parser.parse_args()
+        start_date = args.get('start_date')
+        end_date = args.get('end_date')
         bitcoins = [
             BitcoinStat('11.12.2018', 23.54, 100.10, 98),
-            BitcoinStat('11.13.2018', 22.84, 99.10, 91),
+            BitcoinStat('11.13.2018', 22.84, 99.10, 92),
         ]
+        # bitcoin_stats = get_bitcoin_stats(start_date, end_date)
+        # bitcoin_stats = BitcoinStatsScraper(start_date, end_date).run()
         return {
-            "bitcoinStats": [b.to_json() for b in bitcoins]
+            "bitcoinStats": [b.to_json() for b in bitcoins],
+            "start_date": start_date,
+            "end_date": end_date,
         }
